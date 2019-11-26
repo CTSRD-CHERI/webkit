d52189044cb08f49eeb6d2c901da80562590c987
Ruben Ayrapetyan <ruben.ayrapetyan@arm.com>
2019-11-26 12:47:41 +0000

    CHERI: Get stack pointer with __builtin_cheri_stack_get

--- Source/WTF/wtf/StackPointer.h
+++ Source/WTF/wtf/StackPointer.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2018 Apple Inc. All rights reserved.
+ * Copyright (C) 2019 Arm Ltd. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -44,7 +45,11 @@ ALWAYS_INLINE void* currentStackPointer()
     __asm__ volatile ("mov %0, sp" : "=r"(stackPointerRegister) ::);
     stackPointer = reinterpret_cast<void*>(stackPointerRegister);
 #elif CPU(ARM64) || CPU(ARM_THUMB2) || CPU(ARM_TRADITIONAL)
+#ifdef __CHERI_PURE_CAPABILITY__
+    stackPointer = __builtin_cheri_stack_get();
+#else
     __asm__ volatile ("mov %0, sp" : "=r"(stackPointer) ::);
+#endif
 #endif
     return stackPointer;
 }
