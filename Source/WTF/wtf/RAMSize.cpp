4c2ac947c3cf8ea2bf4d1ff8b0e081422fba21d1
Ruben Ayrapetyan <ruben.ayrapetyan@arm.com>
2019-11-26 12:44:35 +0000

    Guess RAM size for FreeBSD as 512MB

--- Source/WTF/wtf/RAMSize.cpp
+++ Source/WTF/wtf/RAMSize.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012-2019 Apple Inc. All rights reserved.
+ * Copyright (C) 2019 Arm Ltd. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -41,7 +42,7 @@
 
 namespace WTF {
 
-#if OS(WINDOWS)
+#if OS(WINDOWS) || OS(FREEBSD)
 static constexpr size_t ramSizeGuess = 512 * MB;
 #endif
 
@@ -59,6 +60,8 @@ static size_t computeRAMSize()
     struct sysinfo si;
     sysinfo(&si);
     return si.totalram * si.mem_unit;
+#elif OS(FREEBSD)
+    return ramSizeGuess;
 #else
 #error "Missing a platform specific way of determining the available RAM"
 #endif // OS(LINUX)
