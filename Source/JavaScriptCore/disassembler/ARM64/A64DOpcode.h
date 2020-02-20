23785739029b9749bb9ef70c8656c9c4ba65f62b
Ruben Ayrapetyan <ruben.ayrapetyan@arm.com>
2020-02-20 15:49:26 +0000

    Fix an out-of-bounds in A64 disassembler
    Change-Id: Ie747d17c3a6934ba9c75224479bbcd41709c04d7

--- Source/JavaScriptCore/disassembler/ARM64/A64DOpcode.h
+++ Source/JavaScriptCore/disassembler/ARM64/A64DOpcode.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012-2019 Apple Inc. All rights reserved.
+ * Copyright (C) 2020 Arm Ltd. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -703,7 +704,7 @@ public:
     
     const char* format();
 
-    unsigned opNum() { return mBit(); }
+    unsigned opNumber() { return mBit(); } /* bug fixed (PROT_CHERI_BOUNDS) */
     unsigned mBit() { return (m_opcode >> 23) & 0x1; }
     unsigned sBit() { return (m_opcode >> 22) & 0x1; }
     unsigned wBit() { return (m_opcode >> 11) & 0x1; }
