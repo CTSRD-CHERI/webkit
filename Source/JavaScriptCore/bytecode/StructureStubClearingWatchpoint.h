a18ba1164613a7174244e2f0c056c18e4ef257c3
Ruben Ayrapetyan <ruben.ayrapetyan@arm.com>
2020-02-27 13:10:07 +0000

    CHERI: PlainPtr for WatchpointsOnStructureStubInfo
    Change-Id: I455f9b71fc74818439d60672479c4865008f533b

--- Source/JavaScriptCore/bytecode/StructureStubClearingWatchpoint.h
+++ Source/JavaScriptCore/bytecode/StructureStubClearingWatchpoint.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012, 2015 Apple Inc. All rights reserved.
+ * Copyright (C) 2020 Arm Ltd. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -55,7 +56,11 @@ public:
 
 private:
     // Own destructor may not be called. Keep members trivially destructible.
+#if defined(__CHERI_PURE_CAPABILITY__)
+    JSC_WATCHPOINT_FIELD(PlainPtr<WatchpointsOnStructureStubInfo>, m_holder);
+#else
     JSC_WATCHPOINT_FIELD(PackedPtr<WatchpointsOnStructureStubInfo>, m_holder);
+#endif
     JSC_WATCHPOINT_FIELD(ObjectPropertyCondition, m_key);
 };
 
@@ -76,7 +81,11 @@ public:
 
 
 private:
+#if defined(__CHERI_PURE_CAPABILITY__)
+    PlainPtr<WatchpointsOnStructureStubInfo> m_holder;
+#else
     PackedPtr<WatchpointsOnStructureStubInfo> m_holder;
+#endif
 };
 
 class WatchpointsOnStructureStubInfo {
