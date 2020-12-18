CTSRD-CHERI/qtwebkit

Fix many of the intcap_t bitwise operation warnings by either:
1) using the new q{Get,Set,Clear}LowPointerBits functions in QtBase
2) using __builtin_align_{up, down}
3) replacing the intptr_t with an integer type if that is the intention

Source/JavaScriptCore/heap/MarkedBlock.h
@@ -227,12 +227,16 @@ namespace JSC {
 
     inline bool MarkedBlock::isAtomAligned(const void* p)
     {
-        return !(reinterpret_cast<Bits>(p) & atomAlignmentMask);
+        return !qGetLowPointerBits<atomAlignmentMask>(reinterpret_cast<Bits>(p));
     }
 
     inline MarkedBlock* MarkedBlock::blockFor(const void* p)
     {
+#if __has_builtin(__builtin_align_down)
+        return reinterpret_cast<MarkedBlock*>(__builtin_align_down(const_cast<void*>(p), blockSize));
+#else
         return reinterpret_cast<MarkedBlock*>(reinterpret_cast<Bits>(p) & blockMask);
+#endif
     }
 
     inline MarkedAllocator* MarkedBlock::allocator() const

Source/JavaScriptCore/runtime/StackAlignment.h
@@ -33,7 +33,13 @@
 namespace JSC {
 
 // NB. Different platforms may have different requirements here. But 16 bytes is very common.
-inline unsigned stackAlignmentBytes() { return 16; }
+inline unsigned stackAlignmentBytes() {
+#ifdef __CHERI_PURE_CAPABILITY__
+  return (_MIPS_SZCAP/8);
+#else
+  return 16;
+#endif
+}
 
 inline unsigned stackAlignmentRegisters()
 {

CTSRD-CHERI/webkit

Source/JavaScriptCore/heap/MarkedBlock.h
@@ -440,7 +440,7 @@ inline MarkedBlock::Atom* MarkedBlock::atoms()
 
 inline bool MarkedBlock::isAtomAligned(const void* p)
 {
-    return !(reinterpret_cast<uintptr_t>(p) & atomAlignmentMask);
+    return !qGetLowPointerBits<atomAlignmentMask>(reinterpret_cast<uintptr_t>(p));
 }
 
 inline void* MarkedBlock::Handle::cellAlign(void* p)
@@ -455,7 +455,12 @@ inline void* MarkedBlock::Handle::cellAlign(void* p)
 
 inline MarkedBlock* MarkedBlock::blockFor(const void* p)
 {
+#if __has_builtin(__builtin_align_down)
+    return reinterpret_cast<MarkedBlock*>(__builtin_align_down(const_cast<void*>(p), blockSize));
+#else
     return reinterpret_cast<MarkedBlock*>(reinterpret_cast<uintptr_t>(p) & blockMask);
+#endif
+
 }
 
 inline BlockDirectory* MarkedBlock::Handle::directory() const

Source/JavaScriptCore/runtime/StackAlignment.h
@@ -32,7 +32,13 @@
 namespace JSC {
 
 // NB. Different platforms may have different requirements here. But 16 bytes is very common.
-constexpr unsigned stackAlignmentBytes() { return 16; }
+constexpr unsigned stackAlignmentBytes() {
+#ifdef __CHERI_PURE_CAPABILITY__
+  return (_MIPS_SZCAP/8);
+#else
+  return 16;
+#endif
+}
 
 constexpr unsigned stackAlignmentRegisters()
 {