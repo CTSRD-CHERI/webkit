CTSRD-CHERI/qtwebkit

Override << operator for __intcap_t in TextStream

Source/WebCore/platform/text/TextStream.h
@@ -57,10 +57,14 @@ public:
     WEBCORE_EXPORT TextStream& operator<<(unsigned long long);
     WEBCORE_EXPORT TextStream& operator<<(float);
     WEBCORE_EXPORT TextStream& operator<<(double);
+#ifdef __CHERI__
+    WEBCORE_EXPORT TextStream& operator<<(__intcap_t);
+#endif
     WEBCORE_EXPORT TextStream& operator<<(const char*);
     WEBCORE_EXPORT TextStream& operator<<(const void*);
     WEBCORE_EXPORT TextStream& operator<<(const String&);
     WEBCORE_EXPORT TextStream& operator<<(const FormatNumberRespectingIntegers&);
+
 
     template<typename T>
     void dumpProperty(const String& name, const T& value)

CTSRD-CHERI/webkit

Source/WTF/wtf/text/TextStream.h
@@ -67,6 +67,9 @@ public:
     WTF_EXPORT_PRIVATE TextStream& operator<<(unsigned long long);
     WTF_EXPORT_PRIVATE TextStream& operator<<(float);
     WTF_EXPORT_PRIVATE TextStream& operator<<(double);
+#ifdef __CHERI__
+    WTF_EXPORT_PRIVATE TextStream& operator<<(__intcap_t);
+#endif
     WTF_EXPORT_PRIVATE TextStream& operator<<(const char*);
     WTF_EXPORT_PRIVATE TextStream& operator<<(const void*);
     WTF_EXPORT_PRIVATE TextStream& operator<<(const String&);