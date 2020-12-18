CTSRD-CHERI/qtwebkit

Silence numerous compiler warnings about a particular bitwise-and operation. Use __builtin_cheri_address_get to get the virtual address.

Source/JavaScriptCore/runtime/JSCJSValueInlines.h
@@ -489,7 +489,11 @@ inline bool JSValue::isCell() const
 
 inline bool JSValue::isInt32() const
 {
+#ifdef __CHERI_PURE_CAPABILITY__
+    return (__builtin_cheri_address_get(u.ptr) & TagTypeNumber) == TagTypeNumber;
+#else
     return (u.asInt64 & TagTypeNumber) == TagTypeNumber;
+#endif
 }
 
 inline int64_t reinterpretDoubleToInt64(double value)

CTSRD-CHERI/webkit

Source/JavaScriptCore/runtime/JSCJSValueInlines.h
@@ -514,7 +514,11 @@ inline bool JSValue::isCell() const
 
 inline bool JSValue::isInt32() const
 {
+#ifdef __CHERI_PURE_CAPABILITY__
+    return (__builtin_cheri_address_get(u.ptr) & NumberTag) == NumberTag;
+#else
     return (u.asInt64 & NumberTag) == NumberTag;
+#endif
 }
 
 inline int64_t reinterpretDoubleToInt64(double value)